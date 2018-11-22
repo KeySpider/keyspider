996e3a35f6e11b0376eef0c85e4b6413c88c8c8f
macintoshhd <tuanla@nal.vn>
2018-11-22 23:16:03 +0700

    Test include

--- app/Http/Controllers/ImportController.php
+++ app/Http/Controllers/ImportController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Ldaplibs\Import\CSVReader;
 
 class ImportController extends Controller
 {
     public function showFormUpload()
     {
+        $reader = new CSVReader();
+        $reader->test();
         return view('imports.form_upload');
     }
 }
