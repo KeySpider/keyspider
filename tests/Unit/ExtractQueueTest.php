4547c4e79d0178b86c8fb44180b62c24a5ec87fc
Le Anh Tuan <tuanla@nal.vn>
2018-12-21 13:10:34 +0700

    [LDAP-78] Unit test for exporting

--- tests/Unit/ExtractQueueTest.php
+++ tests/Unit/ExtractQueueTest.php
@@ -13,51 +13,6 @@ use Illuminate\Support\Facades\Log;
 use Tests\TestCase;
 use Exception;
 
-/**
- * Copy a file, or recursively copy a folder and its contents
- * @author      Aidan Lister <aidan@php.net>
- * @version     1.0.1
- * @link        http://aidanlister.com/2004/04/recursively-copying-directories-in-php/
- * @param       string $source Source path
- * @param       string $dest Destination path
- * @param       int $permissions New folder creation permissions
- * @return      bool     Returns true on success, false on failure
- */
-
-/*
-function xcopy($source, $dest, $permissions = 0755)
-{
-    // Check for symlinks
-    if (is_link($source)) {
-        return symlink(readlink($source), $dest);
-    }
-
-    // Simple copy for a file
-    if (is_file($source)) {
-        return copy($source, $dest);
-    }
-
-    // Make destination directory
-    if (!is_dir($dest)) {
-        mkdir($dest, $permissions);
-    }
-
-    // Loop through the folder
-    $dir = dir($source);
-    while (false !== $entry = $dir->read()) {
-        // Skip pointers
-        if ($entry == '.' || $entry == '..') {
-            continue;
-        }
-
-        // Deep copy directories
-        xcopy("$source/$entry", "$dest/$entry", $permissions);
-    }
-
-    // Clean up
-    $dir->close();
-    return true;
-}*/
 
 class ExtractQueueTest extends TestCase
 {
@@ -68,6 +23,9 @@ class ExtractQueueTest extends TestCase
      */
     public function testBasicTest()
     {
+        $unittestExportDir = '/Applications/MAMP/htdocs/LDAP_ID/storage/unittest/export/';
+        array_map('unlink', glob($unittestExportDir.'*.*'));
+        sleep(2);
 
         $extractSetting = $this->setDataToDoTest();
         print_r("Do testing for extract");
@@ -77,9 +35,10 @@ class ExtractQueueTest extends TestCase
         }
 
         sleep(2);
-        $unittestExportDir = '/Applications/MAMP/htdocs/LDAP_ID/storage/unittest/export/';
+
         $hogehogeU_time = filemtime('' . $unittestExportDir . 'hogehogeU.csv');
-        $hogehogeO_time = filemtime('/Applications/MAMP/htdocs/LDAP_ID/storage/unittest/export/hogehogeO.csv');
+        $hogehogeO_time = filemtime('' . $unittestExportDir . 'hogehogeO.csv');
+
         $this->assertTrue($hogehogeU_time<=$hogehogeO_time);
     }
 
