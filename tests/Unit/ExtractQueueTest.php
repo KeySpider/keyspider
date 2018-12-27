643af07cd42a99c0f1f479c005aa32f69c8f626e
ngulb <ngulb@tech.est-rouge.com>
2018-12-27 13:33:32 +0700

    Adding license

--- tests/Unit/ExtractQueueTest.php
+++ tests/Unit/ExtractQueueTest.php
@@ -1,4 +1,21 @@
 <?php
+/*******************************************************************************
+ * Key Spider
+ * Copyright (C) 2019 Key Spider Japan LLC
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ ******************************************************************************/
 
 namespace Tests\Unit;
 
@@ -12,7 +29,6 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Tests\TestCase;
 use Exception;
-
 
 class ExtractQueueTest extends TestCase
 {
@@ -92,5 +108,4 @@ class ExtractQueueTest extends TestCase
             Log::error($e);
         }
     }
-
 }
