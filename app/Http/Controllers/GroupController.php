f332c3de941a35822b5b8b198a15b7664faad27f
ngulb <ngulb@tech.est-rouge.com>
2019-01-10 10:57:30 +0700

    Build SCIM lib.

---
+++ app/Http/Controllers/GroupController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Models\GroupResource;
+use App\Http\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Optimus\Bruno\EloquentBuilderTrait;
+use Optimus\Bruno\LaravelController;
+
+class GroupController extends LaravelController
+{
+    use EloquentBuilderTrait;
+
+    public function index(Request $request)
+    {
+        $this->setFilterForRequest($request);
+
+        $resourceOptions = $this->parseResourceOptions($request);
+
+        // Start a new query for books using Eloquent query builder
+        // (This would normally live somewhere else, e.g. in a Repository)
+        $query = User::query();
+        $this->applyResourceOptions($query, $resourceOptions);
+
+        $users = $query->get();
+        $parsedData = $this->parseData($users, $resourceOptions, 'users');
+
+        sleep(1);
+        return $this->response($this->toSCIMArray($parsedData));
+    }
+
+    /**
+     * @param Request $request
+     * @return bool
+     */
+    private function setFilterForRequest(Request &$request): bool
+    {
+        $filter = explode(' ', $request->input('filter'));
+        try {
+            $request["filter_groups"] = array(
+                0 =>
+                    array(
+                        'filters' =>
+                            array(
+                                0 =>
+                                    array(
+                                        'key' => $filter[0],
+                                        'value' => $filter[2],
+                                        'operator' => $filter[1],
+                                    ),
+                            ),
+                    ),
+            );
+            return true;
+        } catch (\Exception $exception) {
+            return false;
+        }
+    }
+
+    public function toSCIMArray($dataArray)
+    {
+        $arr = [
+            'totalResults' => count($dataArray),
+            "itemsPerPage" => 10,
+            "startIndex" => 1,
+            "schemas" => [
+                "urn:ietf:params:scim:api:messages:2.0:ListResponse"
+            ],
+            'Resources' => [],
+        ];
+        return $arr;
+    }
+
+    public function store(Request $request)
+    {
+        Log::info('---------------------------------------------------');
+        Log::info('-----------------creating group...-----------------');
+        Log::info(json_encode($request->all(), JSON_PRETTY_PRINT));
+        Log::info('---------------------------------------------------');
+        sleep(1);
+
+        // import into database
+        GroupResource::create([
+            "data" => json_encode($request->all(), JSON_PRETTY_PRINT),
+        ]);
+
+        return $this->response('{"schemas":["urn:ietf:params:scim:schemas:core:2.0:User"]}');
+    }
+}
