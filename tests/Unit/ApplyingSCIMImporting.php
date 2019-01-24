ee2ccf1755b53fec14b99298d6f517640f328234
ngulb <ngulb@tech.est-rouge.com>
2019-01-25 00:17:47 +0700

    Refactor code.

--- tests/Unit/ApplyingSCIMImporting.php
+++ tests/Unit/ApplyingSCIMImporting.php
@@ -18,7 +18,7 @@
  * along with this program. If not, see http://www.gnu.org/licenses/.
  ******************************************************************************/
 
-namespace Tests\Feature;
+namespace Tests\Unit;
 
 use App\Ldaplibs\Import\ImportSettingsManager;
 use App\Ldaplibs\Import\SCIMReader;
