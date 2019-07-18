8fdf4eb3ee66902a3eb843ffd071db33fe316d6b
tuanleanh <tuanla@nal.vn>
2019-07-18 11:14:22 +0700

    Migrate tables based on MasterDBConf.ini

---
+++ app/Ldaplibs/TablesBuilder.php
@@ -0,0 +1,74 @@
+<?php
+
+
+use Illuminate\Support\Facades\Schema;
+
+class TablesBuilder
+{
+    public function __construct($masterDBConfigPath)
+    {
+        $this->masterDBConfigPath = $masterDBConfigPath;
+    }
+
+    public function readIniFile()
+    {
+        return parse_ini_file($this->masterDBConfigPath, true, INI_SCANNER_RAW);
+    }
+
+    public function buildTables()
+    {
+        $buildSucess = true;
+        $tablesMap = $this->readIniFile();
+//        var_dump($tablesMap);
+        foreach ($tablesMap as $tableDesc) {
+//            var_dump($tableDesc);
+            $tableName = null;
+            $columns = [];
+            foreach ($tableDesc as $key => $value) {
+                if (strpos($key, '.') == false and strpos($value, '.') == false) {
+                    $tableName = $value;
+                } elseif (strpos($value, '.') == true) {
+                    $columnName = explode('.', $value)[1];
+                    $columns[] = $columnName;
+                }
+            }
+
+            if ($tableName and count($columns)) {
+                $destinationTable = ['table_name' => $tableName, 'columns' => $columns];
+                $this->migrateTable($destinationTable);
+            }
+//            return ($destinationTable);
+        }
+
+    }
+
+    /**
+     * @param array $destinationTable
+     */
+    private function migrateTable(array $destinationTable): void
+    {
+        $tableName = $destinationTable['table_name'];
+        $columns = $destinationTable['columns'];
+        $columnsInString = implode("|",$columns);
+
+        if (Schema::hasTable($tableName)) {
+            echo "- Update table: <<<$tableName>>>: [$columnsInString]\n";
+            Schema::table($tableName, function ($table) use ($tableName, $columns) {
+                foreach ($columns as $column) {
+                    if (!Schema::hasColumn($tableName, $column)) {
+                        echo "    + add Column: [$column]\n";
+                        $table->string($column);
+                    }
+                }
+            });
+
+        } else {
+            echo "- Build table: <<<$tableName>>>: [$columnsInString]\n";
+            Schema::create($tableName, function ($table) use ($columns) {
+                foreach ($columns as $column) {
+                    $table->string($column);
+                }
+            });
+        }
+    }
+}
