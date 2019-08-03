5478124b71fad68ebcacf5c6cd5bf4b116bb9c56
usuki@osslabo.com <usuki@osslabo.com>
2019-08-03 09:47:31 +0900

    Fixed errors and tested version.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -57,7 +57,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            'throttle:60,1',
+            'throttle:600,1',
             'bindings',
             'request_logger'
         ],
