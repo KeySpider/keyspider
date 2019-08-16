36ed4b8620d7abcd8c002ed9512e607946c49a65
tuanleanh <tuanla@nal.vn>
2019-08-16 14:33:04 +0700

    json of UpdateFlags should be auto update when insert new export setting files

--- tests/Automation/TestFlow.php
+++ tests/Automation/TestFlow.php
@@ -14,6 +14,7 @@ class TestFlow extends TestCase
     public function __construct($name = null, array $data = [], $dataName = '')
     {
         parent::__construct($name, $data, $dataName);
+        parent::setUp();
         $this->allGroups = [];
     }
 
