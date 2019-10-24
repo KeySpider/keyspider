4a0e078a673518750deb1a1784851adb2eda0996
tuanleanh <tuanla@nal.vn>
2019-10-24 10:47:17 +0700

    Unit test for get user detail, get user list, create user

---
+++ config/GraphAPISchemas.php
@@ -0,0 +1,27 @@
+<?php
+return[
+    'createUserJson' => '{
+  "accountEnabled": true,
+  "city": "Seattle",
+  "country": "United States",
+  "department": "Sales & Marketing",
+  "displayName": "something",
+  "givenName": "something",
+  "jobTitle": "Marketing Director",
+  "mailNickname": "MelissaD",
+  "passwordPolicies": "DisablePasswordExpiration",
+  "passwordProfile": {
+    "password": "Test1234",
+    "forceChangePasswordNextSignIn": false
+  },
+  "officeLocation": "131/1105",
+  "postalCode": "98052",
+  "preferredLanguage": "en-US",
+  "state": "WA",
+  "streetAddress": "9256 Towne Center Dr., Suite 400",
+  "surname": "Darrow",
+  "mobilePhone": "+1 206 555 0110",
+  "usageLocation": "US",
+  "userPrincipalName": "something@naljp.onmicrosoft.com"
+}'
+];
