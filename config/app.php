725fd192c95dacf61b5fdba7927cb5fb255e54c9
takashi terada <takashi.terada.2020@alit.jp>
2020-06-22 09:56:51 +0900

    モジュールの削除（adldap と laravel-oci8） これに伴い手順書の修正が必要になります。

--- config/app.php
+++ config/app.php
@@ -161,7 +161,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Yajra\Oci8\Oci8ServiceProvider::class,
+
         /*
          * Package Service Providers...
          */
@@ -222,7 +222,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Adldap' => Adldap\Laravel\Facades\Adldap::class,
 
     ],
 
