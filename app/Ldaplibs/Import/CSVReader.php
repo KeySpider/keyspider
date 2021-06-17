a017dc74314f3978f95048598902e93a8fba794e
yutaka.sakai.2020@alit.jp <yutaka.sakai.2020@alit.jp>
2021-06-17 10:07:04 +0900

    ID自動採番を実装したことによる ini ファイルの設定変更

--- app/Ldaplibs/Import/CSVReader.php
+++ app/Ldaplibs/Import/CSVReader.php
@@ -198,8 +198,8 @@ class CSVReader
                     $updateFlagsColumnName = $settingManagement->getUpdateFlagsColumnName($aliasTable);
                     $getDataAfterConvert[$updateFlagsColumnName] = $updateFlagsJson;
 
-                    // unset($getDataAfterConvert['ID']);
-                    $getDataAfterConvert['ID'] = $settingManagement->makeIdBasedOnMicrotime($nameTable);
+                    unset($getDataAfterConvert['ID']);
+                    // $getDataAfterConvert['ID'] = $settingManagement->makeIdBasedOnMicrotime($nameTable);
                     DB::table($nameTable)->insert($getDataAfterConvert);
 
                     $regExpManagement->updateUserUpdateFlags($getDataAfterConvert["User_ID"]);
